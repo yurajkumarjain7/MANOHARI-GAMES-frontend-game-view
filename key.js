4d24c662f4e213ce18751d6e9eba1ffcec685686
yurajkumarjain7 <167630544+yurajkumarjain7@users.noreply.github.com>
2025-12-07 14:50:35 +0530

    Add files via upload

---
+++ key.js
@@ -0,0 +1,17 @@
+let key = event.key
+if (key === '' || key ==='') {
+    
+}
+ else {
+alert("sorry there are only three moves")
+console.log("sorry there are only three moves")    
+}
+if(onkeydown = a)
+
+onkeydown => a 
+onkeydown => s
+onkeydown => d 
+
+
+onkeydown => a 
+//function 
